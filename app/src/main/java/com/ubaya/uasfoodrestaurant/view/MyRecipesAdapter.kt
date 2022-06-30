defa1042e6acc8bb895393f6852642a42a181e52
s160419006 <s160419006@student.ubaya.ac.id>
2022-06-30 18:23:11 +0700

    add fragment and adapter my recipe

--- app/src/main/java/com/ubaya/uasfoodrestaurant/view/MyRecipesAdapter.kt
+++ app/src/main/java/com/ubaya/uasfoodrestaurant/view/MyRecipesAdapter.kt
@@ -1,4 +1,65 @@
 package com.ubaya.uasfoodrestaurant.view
 
-class MyRecipesAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import androidx.recyclerview.widget.RecyclerView
+import com.ubaya.uasfoodrestaurant.R
+import com.ubaya.uasfoodrestaurant.databinding.MyrecipeListItemBinding
+import com.ubaya.uasfoodrestaurant.databinding.RecipeListItemBinding
+import com.ubaya.uasfoodrestaurant.model.MyRecipes
+import com.ubaya.uasfoodrestaurant.model.Recipe
+import com.ubaya.uasfoodrestaurant.model.Recipes
+import com.ubaya.uasfoodrestaurant.util.loadImage
+import kotlinx.android.synthetic.main.fragment_my_recipes.view.*
+import kotlinx.android.synthetic.main.myrecipe_list_item.view.*
+import kotlinx.android.synthetic.main.recipe_list_item.view.*
+
+class MyRecipesAdapter(val recipeList:ArrayList<Recipes>): RecyclerView.Adapter<MyRecipesAdapter.MyRecipeViewHolder>(), ButtonEditClickListener, ButtonPIClickListener {
+    class MyRecipeViewHolder(val view: MyrecipeListItemBinding): RecyclerView.ViewHolder(view.root)
+
+    fun updateRecipeList(newRecipeList:List<Recipes>){
+        recipeList.clear()
+        recipeList.addAll(newRecipeList)
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyRecipeViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+//        val v = inflater.inflate(R.layout.myrecipe_list_item, parent, false)
+        val v = DataBindingUtil.inflate<MyrecipeListItemBinding>(inflater,
+            R.layout.myrecipe_list_item, parent, false)
+        return MyRecipeViewHolder(v)
+    }
+
+    override fun onBindViewHolder(holder: MyRecipeViewHolder, position: Int) {
+        with(holder.view){
+            recipe =recipeList[position]
+            editListener = this@MyRecipesAdapter
+            addPIListener = this@MyRecipesAdapter
+            imageUrl = recipeList[position].poster
+            if(recipeList[position].public_stat == 0){
+                btnAddIngPrep.visibility = View.VISIBLE
+                btnEdit.visibility = View.GONE
+            }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return recipeList.size
+    }
+
+    override fun onButtonEditClick(v: View) {
+        val action = MyRecipesFragmentDirections.actionEditFragment(v.tag.toString().toInt()
+        )
+        Navigation.findNavController(v).navigate(action)
+    }
+
+    override fun onButtonPIClick(v: View) {
+        val action = MyRecipesFragmentDirections.actionCreatePIFragment(v.tag.toString().toInt()
+        )
+        Navigation.findNavController(v).navigate(action)
+    }
 }
--- app/src/main/java/com/ubaya/uasfoodrestaurant/view/MyRecipesFragment.kt
+++ app/src/main/java/com/ubaya/uasfoodrestaurant/view/MyRecipesFragment.kt
@@ -5,56 +5,115 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.ubaya.uasfoodrestaurant.R
+import com.ubaya.uasfoodrestaurant.viewmodel.ListViewModel
+import kotlinx.android.synthetic.main.fragment_my_recipes.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [MyRecipesFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class MyRecipesFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    private  lateinit var viewModel: ListViewModel
+    private val myRecipeListAdapter = MyRecipesAdapter(arrayListOf())
+    private  lateinit var viewModel2: ListViewModel
+    private val myRecipeListAdapter2 = MyRecipesAdapter(arrayListOf())
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_my_recipes, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MyRecipesFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MyRecipesFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel = ViewModelProvider(this).get(ListViewModel::class.java)
+        viewModel.refreshMyRecipes()
+
+        viewModel2 = ViewModelProvider(this).get(ListViewModel::class.java)
+        viewModel2.refreshDraft()
+
+        recViewMyRecipe.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+        recViewMyRecipe.adapter = myRecipeListAdapter
+
+        recViewDraft.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+        recViewDraft.adapter = myRecipeListAdapter2
+
+
+
+        SwipeRefreshLayout.setOnRefreshListener {
+            recViewMyRecipe.visibility = View.GONE
+            recViewDraft.visibility = View.GONE
+            txtErrorMyRecipe.visibility = View.GONE
+            progressBarBF.visibility = View.VISIBLE
+            viewModel.refreshMyRecipes()
+            viewModel2.refreshDraft()
+            SwipeRefreshLayout.isRefreshing = false
+        }
+
+        observeViewModel()
+
+//        txtDessert.visibility = View.GONE
+//        recViewDessert.visibility = View.GONE
+    }
+
+    fun observeViewModel(){
+        viewModel.recipessLD.observe(viewLifecycleOwner, Observer {
+            myRecipeListAdapter.updateRecipeList(it)
+        })
+
+        viewModel2.recipesDraftLD.observe(viewLifecycleOwner, Observer {
+            myRecipeListAdapter2.updateRecipeList(it)
+        })
+
+        viewModel.loadingErrorLD.observe(viewLifecycleOwner, Observer {
+            if(it){
+                txtErrorMyRecipe.visibility = View.VISIBLE
             }
+            else{
+                txtErrorMyRecipe.visibility = View.GONE
+            }
+        })
+
+        viewModel.loadingLD.observe(viewLifecycleOwner, Observer {
+            if(it){
+                progressBarBF.visibility = View.VISIBLE
+                recViewMyRecipe.visibility = View.GONE
+                txtMyRecipe.visibility = View.GONE
+                txtDraft.visibility = View.GONE
+            }
+            else{
+                progressBarBF.visibility = View.GONE
+                recViewMyRecipe.visibility = View.VISIBLE
+                txtMyRecipe.visibility = View.VISIBLE
+                txtDraft.visibility = View.VISIBLE
+            }
+        })
+
+        viewModel2.loadingErrorDraftLD.observe(viewLifecycleOwner, Observer {
+            if(it){
+                txtErrorDraft.visibility = View.VISIBLE
+            }
+            else{
+                txtErrorDraft.visibility = View.GONE
+            }
+        })
+
+        viewModel2.loadingDraftLD.observe(viewLifecycleOwner, Observer {
+            if(it){
+                progressBarDS.visibility = View.VISIBLE
+                txtErrorDraft.visibility = View.GONE
+                recViewDraft.visibility = View.GONE
+                txtMyRecipe.visibility = View.GONE
+                txtDraft.visibility = View.GONE
+            }
+            else{
+                progressBarDS.visibility = View.GONE
+                recViewDraft.visibility = View.VISIBLE
+                txtMyRecipe.visibility = View.VISIBLE
+                txtDraft.visibility = View.VISIBLE
+            }
+        })
+
+
     }
 }
