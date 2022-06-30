d01b8afc55deb4dbf45302feffb21377efdb4abf
s160419006 <s160419006@student.ubaya.ac.id>
2022-06-30 18:04:05 +0700

    add fragment and adapter recipe

--- app/src/main/java/com/ubaya/uasfoodrestaurant/view/RecipeListFragment.kt
+++ app/src/main/java/com/ubaya/uasfoodrestaurant/view/RecipeListFragment.kt
@@ -5,31 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.ubaya.uasfoodrestaurant.R
+import com.ubaya.uasfoodrestaurant.viewmodel.ListViewModel
+import kotlinx.android.synthetic.main.fragment_recipe_list.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [RecipeListFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class RecipeListFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
+    private  lateinit var viewModel: ListViewModel
+    private val recipeListAdapter = RecipeListAdapter(arrayListOf())
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -38,23 +23,51 @@ class RecipeListFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_recipe_list, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment RecipeListFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            RecipeListFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel = ViewModelProvider(this).get(ListViewModel::class.java)
+        viewModel.refresh()
+        viewModel.refreshIng()
+        viewModel.refreshPrep()
+
+        recView.layoutManager = LinearLayoutManager(context)
+        recView.adapter = recipeListAdapter
+        refreshLayout.setOnRefreshListener {
+            recView.visibility = View.GONE
+            txtError.visibility = View.GONE
+            progressLoad.visibility = View.VISIBLE
+            viewModel.refresh()
+            refreshLayout.isRefreshing = false
+        }
+
+        observeViewModel()
+
+    }
+
+    fun observeViewModel(){
+        viewModel.recipessLD.observe(viewLifecycleOwner, Observer {
+            recipeListAdapter.updateRecipeList(it)
+        })
+
+        viewModel.loadingErrorLD.observe(viewLifecycleOwner, Observer {
+            if(it){
+                txtError.visibility = View.VISIBLE
             }
+            else{
+                txtError.visibility = View.GONE
+            }
+        })
+
+        viewModel.loadingLD.observe(viewLifecycleOwner, Observer {
+            if(it){
+                progressLoad.visibility = View.VISIBLE
+                recView.visibility = View.GONE
+            }
+            else{
+                progressLoad.visibility = View.GONE
+                recView.visibility = View.VISIBLE
+            }
+        })
     }
 }
