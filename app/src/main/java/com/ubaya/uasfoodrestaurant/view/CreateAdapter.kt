0c45737f31226802778d7ce3b3dc6f6f8409136a
aldys <aldysetiawan335@gmail.com>
2022-06-30 17:55:05 +0700

    update

--- app/src/main/java/com/ubaya/uasfoodrestaurant/view/CreateAdapter.kt
+++ app/src/main/java/com/ubaya/uasfoodrestaurant/view/CreateAdapter.kt
@@ -1,4 +1,32 @@
 package com.ubaya.uasfoodrestaurant.view
 
-class CreateAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.ubaya.uasfoodrestaurant.R
+import com.ubaya.uasfoodrestaurant.model.Grocery
+import com.ubaya.uasfoodrestaurant.model.Recipes
+import com.ubaya.uasfoodrestaurant.util.loadImage
+import kotlinx.android.synthetic.main.fragment_create.view.*
+import kotlinx.android.synthetic.main.grocery_bag_item.view.*
+
+class CreateAdapter(val recipe:ArrayList<Recipes>): RecyclerView.Adapter<CreateAdapter.GroceryViewHolder>() {
+    class GroceryViewHolder(val view: View): RecyclerView.ViewHolder(view)
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GroceryViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val v = inflater.inflate(R.layout.grocery_bag_item, parent, false)
+
+        return GroceryViewHolder(v)
+    }
+
+    override fun onBindViewHolder(holder: GroceryViewHolder, position: Int) {
+
+    }
+
+    override fun getItemCount(): Int {
+        return recipe.size
+    }
 }
--- app/src/main/java/com/ubaya/uasfoodrestaurant/view/CreateFragment.kt
+++ app/src/main/java/com/ubaya/uasfoodrestaurant/view/CreateFragment.kt
@@ -1,60 +1,93 @@
 package com.ubaya.uasfoodrestaurant.view
 
+import android.app.AlertDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.Navigation
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.ubaya.uasfoodrestaurant.R
+import com.ubaya.uasfoodrestaurant.databinding.FragmentCreateBinding
+import com.ubaya.uasfoodrestaurant.model.Ingredients
+import com.ubaya.uasfoodrestaurant.model.MyRecipes
+import com.ubaya.uasfoodrestaurant.model.Recipes
+import com.ubaya.uasfoodrestaurant.util.Notification
+import com.ubaya.uasfoodrestaurant.viewmodel.DetailRecipeViewModel
+import kotlinx.android.synthetic.main.fragment_create.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class CreateFragment : Fragment(), ButtonAddClickListener, RadioClickListener {
+    private lateinit var viewModel: DetailRecipeViewModel
+    private lateinit var dataBinding:FragmentCreateBinding
 
-/**
- * A simple [Fragment] subclass.
- * Use the [CreateFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class CreateFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    var text = ""
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+//        return inflater.inflate(R.layout.fragment_create, container, false)
+        dataBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_create, container, false)
+        return dataBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel = ViewModelProvider(this).get(DetailRecipeViewModel::class.java)
+//        dataBinding.myrecipe = MyRecipes("",4,0,"")
+        dataBinding.recipe = Recipes("","",0,"",0)
+        dataBinding.listener = this
+        dataBinding.radiolistener = this
+        dataBinding.breakfast = "Breakfast"
+        dataBinding.dessert = "Dessert"
+        dataBinding.dinner = "Dinner"
+        dataBinding.drink = "Drink"
+//        dataBinding.ingredient = this.toString()
+//        dataBinding.ingredients = Ingredients(1,"","")
+//        text = dataBinding.ingredients!!.item.toString()
+//        observeViewModel()
+    }
+
+    override fun onButtonAddClick(v: View) {
+
+//        dataBinding.ingredient = this.
+//        observeViewModel()
+//        val test = "1.Pertama;2.Kedua;3.Ketiga;4.Keempat"
+//        var result = dataBinding.ingredient!!.split(";").map { it.trim() }
+//        Toast.makeText(v.context, result.toString(), Toast.LENGTH_SHORT).show()
+        AlertDialog.Builder(context).apply {
+            setMessage("You want to add this recipe?")
+            setPositiveButton("Yes") { _, _ ->
+                Notification(v.context).createNotificationDraft("Recipe Created","A new recipe have been saved into draft")
+                viewModel.addRecipe(dataBinding.recipe!!)
+//                viewModel.selectLastRecipe()
+
+//                viewModel.deleteAllMyRecipe(4)
+//                viewModel.deleteAllMyRecipe(10)
+                Toast.makeText(
+                    context,
+                    dataBinding.recipe!!.toString(),
+                    Toast.LENGTH_SHORT
+                ).show()
+                val action = CreateFragmentDirections.actionBackToListFragment()
+                Navigation.findNavController(v).navigate(action)
+            }
+            setNegativeButton("Cancel", null)
+
+            create().show()
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_create, container, false)
+    override fun onRadioClick(v: View, category: String, obj: Recipes) {
+        obj.category = v.tag.toString()
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment CreateFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            CreateFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+//    override fun onTextChanged(s: CharSequence) {
+//        text = s.toString()
+//    }
 }
