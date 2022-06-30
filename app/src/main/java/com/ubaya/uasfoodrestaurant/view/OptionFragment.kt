3fc51009ba67884cbb487cf4dd13f23323a77416
s160419006 <s160419006@student.ubaya.ac.id>
2022-06-30 18:18:15 +0700

    add fragment opt

--- app/src/main/java/com/ubaya/uasfoodrestaurant/view/OptionFragment.kt
+++ app/src/main/java/com/ubaya/uasfoodrestaurant/view/OptionFragment.kt
@@ -1,60 +1,69 @@
 package com.ubaya.uasfoodrestaurant.view
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.ubaya.uasfoodrestaurant.R
+import com.ubaya.uasfoodrestaurant.databinding.FragmentOptionBinding
+import com.ubaya.uasfoodrestaurant.model.Recipes
+import com.ubaya.uasfoodrestaurant.viewmodel.DetailRecipeViewModel
+import kotlinx.android.synthetic.main.fragment_option.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [OptionFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class OptionFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+class OptionFragment : BottomSheetDialogFragment(), ButtonShareClickListener {
+    private lateinit var viewModel: DetailRecipeViewModel
+    private lateinit var dataBinding: FragmentOptionBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_option, container, false)
+//        return inflater.inflate(R.layout.fragment_option, container, false)
+        dataBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_option, container, false)
+        return dataBinding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment OptionFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            OptionFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel = ViewModelProvider(this).get(DetailRecipeViewModel::class.java)
+
+        val id = OptionFragmentArgs.fromBundle(requireArguments()).id
+        dataBinding.shareListener = this
+
+        viewModel.fetch(id)
+
+        observeViewModel()
+    }
+
+    fun observeViewModel() {
+        viewModel.recipeLD.observe(viewLifecycleOwner, Observer {
+            dataBinding.recipe = it
+            dataBinding.imageUrl = it.poster
+
+        })
+    }
+
+    override fun onButtonShareClick(v: View) {
+        val sendIntent = Intent.createChooser(Intent().apply {
+            action = Intent.ACTION_SEND
+            var text = "Recipe ${dataBinding.recipe!!.name} is a ${dataBinding.recipe!!.category}" +
+                    ", and there are ${dataBinding.recipe!!.likes.toString()} people that like this recipe"
+            var poster = dataBinding.recipe!!.poster
+            putExtra(Intent.EXTRA_TEXT, text)
+            type = "text/plain"
+
+        },null)
+        // Buat share intent
+        val shareIntent = Intent.createChooser(sendIntent, "Kirim pesan menggunakan")
+
+        startActivity(shareIntent)
     }
 }
