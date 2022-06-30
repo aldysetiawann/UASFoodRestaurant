55fd87fc432d483a2dd0f3e3d12bec1ee086edae
s160419006 <s160419006@student.ubaya.ac.id>
2022-06-30 18:13:01 +0700

    add fragment and adapter prep

--- app/src/main/java/com/ubaya/uasfoodrestaurant/view/PreparationListAdapter.kt
+++ app/src/main/java/com/ubaya/uasfoodrestaurant/view/PreparationListAdapter.kt
@@ -1,4 +1,40 @@
 package com.ubaya.uasfoodrestaurant.view
 
-class PreparationListAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.ubaya.uasfoodrestaurant.R
+import com.ubaya.uasfoodrestaurant.databinding.MyrecipeListItemBinding
+import com.ubaya.uasfoodrestaurant.databinding.PreparationListItemBinding
+import com.ubaya.uasfoodrestaurant.model.Preparations
+import kotlinx.android.synthetic.main.ingredient_list_item.view.*
+import kotlinx.android.synthetic.main.preparation_list_item.view.*
+
+class PreparationListAdapter(val preparationList:ArrayList<Preparations>): RecyclerView.Adapter<PreparationListAdapter.PreparationViewHolder>() {
+    class PreparationViewHolder(var view: PreparationListItemBinding) : RecyclerView.ViewHolder(view.root)
+
+    fun updatePreparationList(newPreparationList:List<Preparations>){
+        preparationList.clear()
+        preparationList.addAll(newPreparationList)
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PreparationViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val v = DataBindingUtil.inflate<PreparationListItemBinding>(inflater,
+            R.layout.preparation_list_item, parent, false)
+        return PreparationViewHolder(v)
+    }
+
+    override fun onBindViewHolder(holder: PreparationViewHolder, position: Int) {
+        with(holder.view){
+            preparations = preparationList[position]
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return preparationList.size
+    }
 }
--- app/src/main/java/com/ubaya/uasfoodrestaurant/view/PreparationListFragment.kt
+++ app/src/main/java/com/ubaya/uasfoodrestaurant/view/PreparationListFragment.kt
@@ -1,35 +1,21 @@
 package com.ubaya.uasfoodrestaurant.view
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.ubaya.uasfoodrestaurant.R
+import com.ubaya.uasfoodrestaurant.viewmodel.ListViewModel
+import kotlinx.android.synthetic.main.fragment_preparation_list.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [PreparationListFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class PreparationListFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
+    private  lateinit var viewModel: ListViewModel
+    private val preparationListAdapter = PreparationListAdapter(arrayListOf())
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -38,23 +24,47 @@ class PreparationListFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_preparation_list, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment PreparationListFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            PreparationListFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        if(arguments!=null){
+//            var id = IngredientListFragmentArgs.fromBundle(requireArguments()).id
+        }
+        viewModel = ViewModelProvider(this).get(ListViewModel::class.java)
+        viewModel.refreshPrep(PreparationListFragmentArgs.fromBundle(requireArguments()).id)
+
+
+        Log.d("SHOW ID", IngredientListFragmentArgs.fromBundle(requireArguments()).id.toString())
+
+        recView4.layoutManager = LinearLayoutManager(context)
+        recView4.adapter = preparationListAdapter
+
+        observeViewModel()
+    }
+
+    fun observeViewModel(){
+        viewModel.preparationsLD.observe(viewLifecycleOwner, Observer {
+            preparationListAdapter.updatePreparationList(it)
+        })
+
+        viewModel.loadingErrorLD.observe(viewLifecycleOwner, Observer {
+            if(it){
+                textError.visibility = View.VISIBLE
             }
+            else{
+                textError.visibility = View.GONE
+            }
+        })
+
+        viewModel.loadingLD.observe(viewLifecycleOwner, Observer {
+            if(it){
+                progressBar6.visibility = View.VISIBLE
+                recView4.visibility = View.GONE
+            }
+            else{
+                progressBar6.visibility = View.GONE
+                recView4.visibility = View.VISIBLE
+            }
+        })
     }
 }
