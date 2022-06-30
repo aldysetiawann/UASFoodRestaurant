89045fb27989a9e408b4a7221493a67b4d7be13f
s160419006 <s160419006@student.ubaya.ac.id>
2022-06-30 18:32:28 +0700

    add create

--- app/src/main/java/com/ubaya/uasfoodrestaurant/view/CreatePrapAndIngFragment.kt
+++ app/src/main/java/com/ubaya/uasfoodrestaurant/view/CreatePrapAndIngFragment.kt
@@ -1,60 +1,83 @@
 package com.ubaya.uasfoodrestaurant.view
 
+import android.app.AlertDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.Navigation
 import com.ubaya.uasfoodrestaurant.R
+import com.ubaya.uasfoodrestaurant.databinding.FragmentCreatePrepAndIngBinding
+import com.ubaya.uasfoodrestaurant.model.Ingredients
+import com.ubaya.uasfoodrestaurant.model.Preparations
+import com.ubaya.uasfoodrestaurant.util.NotificationHelper
+import com.ubaya.uasfoodrestaurant.viewmodel.DetailRecipeViewModel
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class CreatePrepAndIngFragment : Fragment(), ButtonAddPIClickListener {
+    private lateinit var viewModel: DetailRecipeViewModel
+    private lateinit var dataBinding: FragmentCreatePrepAndIngBinding
 
-/**
- * A simple [Fragment] subclass.
- * Use the [CreatePrapAndIngFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class CreatePrapAndIngFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    var recipeId = 0
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_create_prap_and_ing, container, false)
+        dataBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_create_prep_and_ing, container, false)
+        return dataBinding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment CreatePrapAndIngFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            CreatePrapAndIngFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(this).get(DetailRecipeViewModel::class.java)
+        dataBinding.ingredients = Ingredients(0,"","")
+        dataBinding.preparations = Preparations(0,0,"")
+
+        dataBinding.listener = this
+
+        if(arguments!=null){
+            recipeId = CreatePrepAndIngFragmentArgs.fromBundle(requireArguments()).id
+        }
+    }
+
+    override fun onButtonAddPIClick(v: View) {
+        var result = dataBinding.ingredient!!.split(";").map { it.trim() }
+        var result2 = dataBinding.amount!!.split(";").map { it.trim() }
+        var result3 = dataBinding.preparation!!.split(";").map { it.trim() }
+//        var arrayIng = arrayListOf<Ingredients>()
+        AlertDialog.Builder(context).apply {
+            setMessage("You want to set public this recipe?")
+            setPositiveButton("Yes") { _, _ ->
+                if(result.size != result2.size){
+                    Toast.makeText(
+                        context,
+                        "The ingredient and ingredient amount need to be the same amount",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                else {
+                    NotificationHelper(v.context).createNotificationPublic("Recipe Uloaded","A new recipe have been public")
+                    for ((index, value) in result.withIndex()) {
+                        viewModel.addIngredient(Ingredients(recipeId, value, result2[index]))
+                    }
+
+                    for ((index, value) in result3.withIndex()) {
+//                        viewModel.addIngredient(Ingredients(recipeId, value, result2[index]))
+                        viewModel.addPreparation(Preparations(recipeId, index+1, value))
+                    }
+                    viewModel.setPublicRecipe(recipeId)
+                    val action = CreatePrepAndIngFragmentDirections.actionBackToMyRecipesFragment()
+                    Navigation.findNavController(v).navigate(action)
                 }
             }
+            setNegativeButton("Cancel", null)
+
+            create().show()
+        }
     }
 }
